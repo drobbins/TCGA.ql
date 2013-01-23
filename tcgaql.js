f63686f8aa2fcdca74154bf00023de3a460182e3
David Robbins <robbins.david@gmail.com>
2013-01-23 13:37:34 -0600

    Added documentation tab.

--- tcgaql.js
+++ tcgaql.js
@@ -155,4 +155,13 @@
 
   TCGA.__defineGetter__("ql", ql);
 
+  TCGA.ui.registerTab({
+    id : "tcga-ql",
+    title : "TCGA.ql Docs",
+    content : '<h1 id="tcga.ql">TCGA.ql</h1><p>TCGA.ql is a prototype query builder and DSL for finding data files in the TCGA’s open access HTTP repository, built as a component for the <a href="https://github.com/tcga/web-toolbox">TCGA Toolbox</a>.</p><p><a href="http://tcga.github.com/?module=https://raw.github.com/drobbins/TCGA.ql/master/tcgaql.js">Load TCGA.ql in TCGA Toolbox</a></p><h1 id="usage">Usage</h1><p>First, create a query object:</p><pre><code>// Using the TCGA.ql getter\nquery=TCGA.ql;\n\n// Alternately, use the "new"keyword\nquery=new TCGA.Query;</code></pre><p>Add constraints to the object:</p><pre><code>query.diseases(["brca", "gbm"]).dataTypes("protein_exp");</code></pre><p>Run the query:</p><pre><code>results=query.run();\n\n// The results object is a jQuery promise\nresults.done( function (){processResults(results);});\n\n// Promise callbacks may also take arguments\nresults.done( function (data){processData(data);});</code></pre><p>To see the list of possible constraints for a given filter, run the filter without any arguments:</p><pre><code>allDiseases=q.diseases();\ndiseasesWithRPPAData=q.dataTypes("protein_exp").diseases();\n\n// These lists of constraints are also jQuery promises\ndiseasesWithRPPAData.done(function (){//...});</code></pre><h2 id="queryq">Query (q)</h2><h3 id="q.filternames">q.filterNames</h3><p>Array containing the list of filters available for this query.</p><h3 id="q.filter_nameconstraints">q.<filter_name>( constraints )</h3><p>Add the named filter to the query. <code>constraints</code> may be a string or array of strings. Passing an empty array clears the filter.</p><p>Returns:</p><ul><li>If no <code>constraints</code> are given, an jQuery promise for all possible constraints on this filter is returned.</li><li>Otherwise, returns the query object, allowing filters to be chained.</li></ul><h3 id="q.querystring">q.queryString()</h3><p>Returns the current SPARQL query representing the active filters.</p><h3 id="q.printquery">q.printQuery()</h3><p><code>console.log</code> the current SPARQL query.</p><h3 id="q.run">q.run()</h3><p>Execute the current SPARQL query, returning a <a href="http://www.w3.org/TR/sparql11-results-json/#json-result-object"><code>application/sparql-results+json</code></a> with the results.</p><p>The returned object has an a jQuery promise as it’s prototype.</p>',
+    switchTab : true
+  }, function () {
+    console.log("TCGA.ql Loaded.");
+  });
+
 })();
